1eb1496a7976d0ff3597a2ed5fdfac5a20b0829b
Robert Lee <bobbydwise1@hotmail.com>
2019-08-12 16:24:51 -0700

    Add function for checking simple substitution.

--- scripts.js
+++ scripts.js
@@ -8,7 +8,27 @@ The edit distance between two strings refers to the minimum number of character 
 Given two strings, compute the edit distance between them.
 */
 
+const compareStrings = (string1, string2) => {
+  let count = 0;
+  let temp;
+  if (string1.length < string2.length) {
+    temp = string1;
+    string1 = string2;
+    string2 = temp;
+    }
+  for (i=0; i<string1.length; i++) {
+    console.log("loop # ", i, ", (x,y): ", string1[i],", ", string2[i])
+    if (string1[i] != string2[i]) {
+      count++;
+    }
+    console.log("count is = ", count)
+  }
+  return count;
+}
 
+let test1 = "kitten";
+let test2 = "sitting";
+console.log(compareStrings(test1,test2))
 
 $(document).ready(function() {
   $('#form1').submit(function(event) {
